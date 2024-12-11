a9d803913816720e631510baa922c8ad6d28515d
Aachal <aachalmahule2017@gmail.com>
2024-12-11 11:30:03 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Weather from './components/Weather.jsx'
+
+function App() {
+  return (
+    <div className='app'>
+      <Weather/>
+      
+      </div>
+  )
+}
+
+export default App
